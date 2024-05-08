940326945e359ca4d347b15f2c4e62d47cb25668
joominse0 <sally51501512@gmail.com>
2024-05-08 15:05:50 +0900

    fix: lineup오류

--- src/pages/Lineup/Lineup.js
+++ src/pages/Lineup/Lineup.js
@@ -8,7 +8,7 @@ function Lineup() {
     const [selectedDay, setSelectedDay] = useState(null);
     let navigate = useNavigate();
 
-<<<<<<< HEAD
+
     const handleDayClick = (day) => {
         setSelectedDay(day);
     }
@@ -73,37 +73,7 @@ function Lineup() {
                         <b style={{color: '#5A776D', fontSize: '24px', marginTop: '0px'}}>{day.split(' ')[1]}</b>
                     </button>
                 ))}
-            </div>
-            {selectedDay && renderContent()}
-=======
-    return (
-        <div className="iphone-frame">
-            <p className="login-title">공연 라인업</p>
-            <img
-                src="../../../img/close.png"
-                alt="취소버튼"
-                className="close-btn"
-                style={{ marginTop: "9%" }}
-                onClick={() => navigate(-1)}
-            />
-
-            <div className="month">May</div>
-
-            <div>
-                <p>Wed</p>
-                <b>24</b>
-            </div>
-
-            <div>
-                <p>Thu</p>
-                <b>25</b>
-            </div>
-
-            <div>
-                <p>Fri</p>
-                <b>26</b>
-            </div>
->>>>>>> main
+            </div>                        {selectedDay && renderContent()}
         </div>
     );
 }
