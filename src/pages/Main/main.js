eb51fc268720264f3e54862aab6afd55475b4558
joominse0 <sally51501512@gmail.com>
2024-05-20 09:37:22 +0900

    feat: 메인 디자인 수정

--- src/pages/Main/main.js
+++ src/pages/Main/main.js
@@ -10,8 +10,9 @@ function Main() {
     const [activeBox, setActiveBox] = useState('');
     const [isSidebarVisible, setIsSidebarVisible] = useState(false);
     const [selectedBooth, setSelectedBooth] = useState('학과부스');
-    const [todayVisitCount, setTodayVisitCount] = useState(0);  // 오늘 접속자 수 카운트
-    const [totalVisitCount, setTotalVisitCount] = useState(0);  //누적 접속자 수 카운트
+    const [todayVisitCount, setTodayVisitCount] = useState(0);
+    const [totalVisitCount, setTotalVisitCount] = useState(0);
+    const [loggedIn, setLoggedIn] = useState(false);
     let navigate = useNavigate();
     const [error, setError] = useState('');
 
@@ -22,16 +23,18 @@ function Main() {
                 'ngrok-skip-browser-warning': '69420',
             }
         })
-            .then(response => {
-                setTodayVisitCount(response.data.todayVisitCount);
-                setTotalVisitCount(response.data.totalVisitCount);
-            })
-            .catch(error => {
-                console.error('접속자 수 에러:', error);
-                setError('접속자 수를 불러오는데 실패했습니다.');
-            });
-    }, []);
+        .then(response => {
+            setTodayVisitCount(response.data.todayVisitCount);
+            setTotalVisitCount(response.data.totalVisitCount);
+        })
+        .catch(error => {
+            console.error('접속자 수 에러:', error);
+            setError('접속자 수를 불러오는데 실패했습니다.');
+        });
 
+        const token = sessionStorage.getItem('token');
+        setLoggedIn(!!token);
+    }, []);
 
     const handleBoxClick = (boxName) => {
         setActiveBox(boxName);
@@ -40,6 +43,7 @@ function Main() {
     const toggleSidebar = () => {
         setIsSidebarVisible(!isSidebarVisible);
     };
+
     const handleBoothClick = (boothName) => {
         setSelectedBooth(boothName);
         navigate('/booth', { state: { selectedCategory: boothName } });
@@ -49,8 +53,7 @@ function Main() {
         '학과부스': {
             img: 'box1icon.png',
             title: '학과부스',
-            description: '학과별 부스의 위치를 확인할 수 있으며 ' +
-                '고양이 스템프를 모으는 데에 유용해요'
+            description: '학과별 부스의 위치를 확인할 수 있으며 고양이 스템프를 모으는 데에 유용해요'
         },
         '푸드트럭': {
             img: 'box2icon.png',
@@ -71,18 +74,27 @@ function Main() {
 
     return (
         <div className="iphone-frame1">
-            <img style={{ position: "absolute", marginTop: "-180%" }} src='../../img/swuniforest.png' />
+            <img style={{ position: "absolute", marginTop: "-180%", width:"144px", height:"15px"}} src='../../img/swuniforest.png' />
             <button className='StampBtn' onClick={() => navigate('/stamp')}>
                 <img style={{ width: "55px", height: "55px", cursor: "pointer" }} src='../../../img/StampBtn.png' alt="Stamp Button"></img>
             </button>
             <button className="sidebar-button" onClick={toggleSidebar}><SidebarIcon /></button>
             {isSidebarVisible && <Sidebar style={{ zIndex: "10" }} onToggle={toggleSidebar} />}
+            {!loggedIn && (
+                <button onClick={() => navigate('/login1')} className="myinfo-button"><Myinfo /></button>
+            )}
+            <div className='background-circle'></div>
+            <div className='small-circle'></div>
+            <img className='cat1' src='../../../img/cat1.png'/>
+            <img className='cat2' src='../../../img/cat2.png'/>
+            <img className='cat3' src='../../../img/cat3.png'/>
+            <img className='cat4' src='../../../img/cat4.png'/>
+            <img className='cat5' src='../../../img/cat5.png'/>
+            <img className='cat6' src='../../../img/cat6.png'/>
+            <div className='swuni-text'>고양이 스탬프를<br/>모아 보아요</div>
 
-            <button onClick={() => navigate('/login1')} className="myinfo-button"><Myinfo /></button>
-
-            <img className='background' src="../../../img/Background.png" alt="배경"
-                style={{ width: "349px", height: "355px", position: "absolute" }} />
-
+            {/* <img className='background' src="../../../img/Background.png" alt="배경"
+                style={{ width: "349px", height: "355px", position: "absolute" }} /> */}
             <div className='btn-list'>
                 {Object.keys(booths).map((booth, index, array) => (
                     <b key={booth} onClick={() => setSelectedBooth(booth)}
@@ -94,7 +106,6 @@ function Main() {
                     </b>
                 ))}
             </div>
-
             <button className='box1' onClick={() => handleBoothClick(selectedBooth)}>
                 <b style={{ color: "#ffffff", marginTop: "20%", marginLeft: "-30%", position: "absolute" }}>{booths[selectedBooth].title}</b>
                 <img src={`../../img/${booths[selectedBooth].img}`} />
@@ -103,10 +114,7 @@ function Main() {
                     <p style={{ color: "#9D9FA4" }}>{booths[selectedBooth].description}</p>
                 </div>
             </button>
-
-
             <p className='visitText'>접속자수</p>
-
             {error && <div className="error-message">{error}</div>}
             <div className='visitor-layout'>
                 <div style={{ fontSize: "12px" }}>오늘 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {todayVisitCount} 명 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; •
@@ -115,7 +123,6 @@ function Main() {
             <div className='madeby'>
                 <p>서울여자대학교 멋쟁이사자처럼 12TH</p>
             </div>
-
         </div>
     );
 }
