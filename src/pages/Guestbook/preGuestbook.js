eae1bcd151521aee74e509169dc54539f5feb7a2
gusdk45 <gusdk1418@naver.com>
2024-05-21 02:16:05 +0900

    Dd

--- src/pages/Guestbook/preGuestbook.js
+++ src/pages/Guestbook/preGuestbook.js
@@ -82,11 +82,11 @@ function PreGuestbook() {
                     visitData ? (
                         <div className="divform1-1">
                             <div style={{ textAlign: 'center' }}>
-                                <b style={{ color: "#9D9FA4", fontSize: "18px" }}>{visitData.username} 님의 학과</b> <br/>
-                                <b style={{ color: "#898A8D", fontSize: "19px" }}>{visitData.major}<span style={{ color: "#9D9FA4" }}>의</span> 방문율</b><br/>
+                                <b style={{ color: "#9D9FA4", fontSize: "18px" }}>{visitData.username} 님의 학과</b> <br />
+                                <b style={{ color: "#898A8D", fontSize: "19px" }}>{visitData.major}<span style={{ color: "#9D9FA4" }}>의</span> 방문율</b><br />
                                 <b style={{ fontSize: "24px", color: "#898A8D", marginRight: "10%" }}>{visitData.visitRate}% </b>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="1" height="22" viewBox="0 0 1 21" fill="none">
-                                    <path d="M0.5 1L0.5 20" stroke="#C1C2C5" strokeLinecap="round"/>
+                                    <path d="M0.5 1L0.5 20" stroke="#C1C2C5" strokeLinecap="round" />
                                 </svg>
                                 <b style={{ fontSize: "24px", color: "#898A8D", marginLeft: "10%" }}>{visitData.rank}위</b>
                             </div>
@@ -94,7 +94,7 @@ function PreGuestbook() {
                     ) : (
                         <div className="divform1">
                             <img src="../../../img/Circle.png"
-                            style={{ width: "106px", position: "relative", marginTop: "15px", marginLeft: "20px" }} />
+                                style={{ width: "106px", position: "relative", marginTop: "15px", marginLeft: "20px" }} />
                             <b style={{ marginTop: "40px", marginLeft: "40px", color: "#898A8D", position: "absolute" }}>우리 학과는 지금 방문율 몇 위?</b>
                             <p style={{ marginTop: "-63px", marginLeft: "43%", fontSize: "10px", color: "#898A8D", position: "absolute" }}>방문하기 버튼을 클릭하면 방문율을 높일 수 있어요</p>
                             <button className="guestbookBtn" onClick={handleVisit}>
@@ -117,7 +117,7 @@ function PreGuestbook() {
             </div>
 
             <a onClick={() => navigate('/visitorRanking')}
-                style={{marginTop:"10px", marginBottom:"10px", marginRight:"-50%", cursor:"pointer"}}>
+                style={{ marginTop: "10px", marginBottom: "10px", marginRight: "-50%", cursor: "pointer" }}>
                 방문율랭킹 확인하기
                 <img style={{ width: "14px", height: "15px" }} src="../../../img/arrow.png"></img>
             </a>
@@ -125,7 +125,9 @@ function PreGuestbook() {
             <div className="divform2">
                 {guestbookEntries.map((entry, index) => (
                     <div key={index} className="guestbook-entry">
-                        <p className="username">{entry.guestName}</p>
+                        <p className="username">
+                            {entry.anonymous ? "익명" : entry.guestName}
+                        </p>
                         <img src={entry.fileName} alt="방명록" className="guestbook-img" />
                         <p className="guestbook-content">{entry.guestContent}</p>
                     </div>
